e3d2cc8cba2ecdddc9983b7ce5fabc95568a1515
adsr71 <adsr71@web.de>
2025-02-12 17:19:21 +0100

    updated birdid docker image

--- runModel.py
+++ runModel.py
@@ -9,13 +9,13 @@ outputRootDir = rootDir + 'TestOutputsTemp/'
 ## Choose model
 
 #modelID = 'birdnet_v2.4'
-modelID = 'birdnet_v2.2'
+#modelID = 'birdnet_v2.2'
 
 #modelID = 'avesecho_v1.3.0'
 #modelID = 'avesecho_v1.3.0_transformer'
 
 #modelID = 'birdid-europe254-medium'
-#modelID = 'birdid-europe254-large'
+modelID = 'birdid-europe254-large'
 
 
 
@@ -62,14 +62,14 @@ def getModelResults(modelID, outputRootDir, listOfFilePathsOrFolder, instanceInd
     if modelID == 'birdid-europe254-medium':
         dockerInputDir = '/input'
         dockerOutputDir = '/output'
-        #dockerParamString = "--gpus device=0 --ipc=host birdid-europe254-v250119-1 python inference.py -i /input -o /output --fileOutputFormats labels_csv --minConfidence 0.01 --csvDelimiter , --sortSpecies --nameType sci --includeFilePathInOutputFiles --modelSize medium"
-        dockerParamString = "--gpus device=0 --ipc=host ghcr.io/mfn-berlin/birdid-europe254-v250119-1 python inference.py -i /input -o /output --fileOutputFormats labels_csv --minConfidence 0.01 --csvDelimiter , --sortSpecies --nameType sci --includeFilePathInOutputFiles --modelSize medium"
+        #dockerParamString = "--gpus device=0 --ipc=host birdid-europe254-v250212-1 python inference.py -i /input -o /output --fileOutputFormats labels_csv --minConfidence 0.01 --csvDelimiter , --sortSpecies --nameType sci --includeFilePathInOutputFiles --modelSize medium"
+        dockerParamString = "--gpus device=0 --ipc=host birdid-europe254-v250212-1 python inference.py -i /input -o /output --fileOutputFormats labels_csv --minConfidence 0.01 --csvDelimiter , --sortSpecies --nameType sci --includeFilePathInOutputFiles --modelSize medium"
 
     if modelID == 'birdid-europe254-large':
         dockerInputDir = '/input'
         dockerOutputDir = '/output'
-        #dockerParamString = "--gpus device=0 --ipc=host birdid-europe254-v250119-1 python inference.py -i /input -o /output --fileOutputFormats labels_csv --minConfidence 0.01 --csvDelimiter , --sortSpecies --nameType sci --includeFilePathInOutputFiles --modelSize large"
-        dockerParamString = "--gpus device=0 --ipc=host ghcr.io/mfn-berlin/birdid-europe254-v250119-1 python inference.py -i /input -o /output --fileOutputFormats labels_csv --minConfidence 0.01 --csvDelimiter , --sortSpecies --nameType sci --includeFilePathInOutputFiles --modelSize large"
+        #dockerParamString = "--gpus device=0 --ipc=host birdid-europe254-v250212-1 python inference.py -i /input -o /output --fileOutputFormats labels_csv --minConfidence 0.01 --csvDelimiter , --sortSpecies --nameType sci --includeFilePathInOutputFiles --modelSize large"
+        dockerParamString = "--gpus device=0 --ipc=host ghcr.io/mfn-berlin/birdid-europe254-v250212-1 python inference.py -i /input -o /output --fileOutputFormats labels_csv --minConfidence 0.01 --csvDelimiter , --sortSpecies --nameType sci --includeFilePathInOutputFiles --modelSize large"
 
 
 

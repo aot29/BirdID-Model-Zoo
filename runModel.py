0453ad5ed5585248c53dc0e2f713e4195bd3ed5a
adsr71 <adsr71@web.de>
2025-02-19 19:37:16 +0100

    script args and batch processing

--- runModel.py
+++ runModel.py
@@ -1,14 +1,13 @@
 import os
 import pandas as pd
+from multiprocessing import cpu_count, freeze_support, Pool
+import argparse
 
 
-# Define paths
-rootDir = os.path.dirname(os.path.abspath(__file__)) + '/'
 
-outputRootDir = rootDir + 'TestOutputsTemp/'
+## Default values
 
-
-## Choose model
+# Model ID
 
 #modelID = 'birdnet_v2.4'
 #modelID = 'birdnet_v2.2'
@@ -20,44 +19,52 @@ modelID = 'birdid-europe254-medium'
 #modelID = 'birdid-europe254-large'
 
 
+# Output root directory
+rootDir = os.path.dirname(os.path.abspath(__file__)) + '/'
+outputRootDir = rootDir + 'TestOutputsTemp/'
+
+# Path to text file with list of file paths
+inputTextFilePath = rootDir + 'inputPaths.txt'
+
+# Docker config
 dockerConfig = {
     'birdnet_v2.4': {
-        'options': '',
+        'options': '--shm-size=4g --ipc=host',
         'inputDir': '/input',
         'outputDir': '/output',
         'image': 'ghcr.io/mfn-berlin/birdnet-v24',
-        'command': '-m birdnet_analyzer.analyze --i input --o output --min_conf 0.01 --overlap 2.0 --rtype csv'
+        'command': '-m birdnet_analyzer.analyze --i input --o output --min_conf 0.01 --overlap 1.0 --rtype csv'
     },
     'birdnet_v2.2': {
-        'options': '',
+        'options': '--shm-size=4g --ipc=host',
         'inputDir': '/input',
         'outputDir': '/output',
         'image': 'ghcr.io/mfn-berlin/birdnet-v22',
-        'command': '-m birdnet_analyzer.analyze --i input --o output --min_conf 0.01 --overlap 2.0 --rtype csv'
+        'command': '-m birdnet_analyzer.analyze --i input --o output --min_conf 0.01 --overlap 1.0 --rtype csv'
     },
     'avesecho_v1.3.0': {
-        'options': '--shm-size=4g --gpus all -e CUDA_VISIBLE_DEVICES=0',
+        'options': '--shm-size=4g --ipc=host --gpus device=0',
         'inputDir': '/app/audio',
         'outputDir': '/app/outputs',
         'image': 'registry.gitlab.com/arise-biodiversity/dsi/algorithms/avesecho-v1/avesechov1:v1.3.0',
         'command': "--i audio --model_name 'fc' --add_csv --mconf 0.01"
     },
     'avesecho_v1.3.0_transformer': {
-        'options': '--shm-size=4g --gpus all -e CUDA_VISIBLE_DEVICES=0',
+        'options': '--shm-size=4g --ipc=host --gpus device=0',
         'inputDir': '/app/audio',
         'outputDir': '/app/outputs',
         'image': 'registry.gitlab.com/arise-biodiversity/dsi/algorithms/avesecho-v1/avesechov1:v1.3.0',
         'command': "--i audio --model_name 'passt' --add_csv --mconf 0.01"
     },
     'birdid-europe254-medium': {
-        'options': '--gpus device=0 --ipc=host',
+        'options': '--shm-size=4g --ipc=host --gpus device=0',
         'inputDir': '/input',
         'outputDir': '/output',
         'image': 'ghcr.io/mfn-berlin/birdid-europe254-v250212-1',
         'command': 'python inference.py -i /input -o /output --fileOutputFormats labels_csv --minConfidence 0.01 --csvDelimiter , --sortSpecies --nameType sci --includeFilePathInOutputFiles --modelSize medium'
     },
     'birdid-europe254-large': {
-        'options': '--gpus device=0 --ipc=host',
+        'options': '--shm-size=4g --gpus device=0 --ipc=host',
         'inputDir': '/input',
         'outputDir': '/output',
         'image': 'ghcr.io/mfn-berlin/birdid-europe254-v250212-1',
@@ -66,18 +73,18 @@ dockerConfig = {
 }
 
 
-# Get some input files
-inputDir = rootDir + 'TestFiles/'
+# # Get some input files
+# inputDir = rootDir + 'TestFiles/'
 
-filePaths = [
-    inputDir + 'LusMeg00027.mp3',
-    inputDir + 'LusMeg00028.mp3'
-    ]
-
-
+# filePaths = [
+#     inputDir + 'LusMeg00027.mp3',
+#     inputDir + 'LusMeg00028.mp3'
+#     ]
 
 
-def getModelResults(modelID, outputRootDir, listOfFilePathsOrFolder, instanceIndex=None, removeContainer=True):
+
+
+def getModelResults(modelID, outputRootDir, listOfFilePathsOrFolder, workerIx=None, nFilesPerBatch=100, removeContainer=True):
 
     if modelID not in dockerConfig:
         raise ValueError(f"Unknown modelID: {modelID}")
@@ -89,38 +96,65 @@ def getModelResults(modelID, outputRootDir, listOfFilePathsOrFolder, instanceInd
 
     ## Define Docker run command string depending on model
 
-
-    
-    ## Create inputMounts depending on whether listOfFilePathsOrFolder is a list of files or a folder
-    # Check if listOfFilePathsOrFolder is a list of files or a folder
-    if isinstance(listOfFilePathsOrFolder, str) and os.path.isdir(listOfFilePathsOrFolder):
-        inputMounts = ' -v ' + listOfFilePathsOrFolder + ':' + dockerConfig[modelID]['inputDir']
-    else:
-        # Construct the Docker run command with bind mounts for each file
-        inputMounts = ''
-        for filePath in listOfFilePathsOrFolder:
-            fileName = os.path.basename(filePath)
-            inputMounts += ' -v ' + filePath + ':' + dockerConfig[modelID]['inputDir'] + '/' + fileName # + '/' is important here
-
-    
     dockerCommand = 'docker run --name ' + modelID
     
     # Add instance index to name if multiple instances of the same model are run in parallel
-    if instanceIndex is not None: 
-        dockerCommand += '_' + str(instanceIndex)
+    if workerIx is not None: 
+        dockerCommand += '_' + str(workerIx)
     # Add option to remove container after run
     if removeContainer: 
         dockerCommand += ' --rm'
 
-    dockerCommand += inputMounts
-    dockerCommand += ' -v ' + outputDir + ':' + dockerConfig[modelID]['outputDir']
-    dockerCommand += ' ' + dockerConfig[modelID]['options']
-    dockerCommand += ' ' + dockerConfig[modelID]['image']
-    dockerCommand += ' ' + dockerConfig[modelID]['command']
 
-    print('dockerCommand:\n', dockerCommand)
-    print('Length of docker command: ' + str(len(dockerCommand)))
-    os.system(dockerCommand)
+    
+    ## Create inputMounts depending on whether listOfFilePathsOrFolder is a list of files or a folder
+    
+    # listOfFilePathsOrFolder is a folder
+    if isinstance(listOfFilePathsOrFolder, str) and os.path.isdir(listOfFilePathsOrFolder):
+        inputMounts = ' -v ' + listOfFilePathsOrFolder + ':' + dockerConfig[modelID]['inputDir']
+
+        dockerCommand += inputMounts
+        dockerCommand += ' -v ' + outputDir + ':' + dockerConfig[modelID]['outputDir']
+        dockerCommand += ' ' + dockerConfig[modelID]['options']
+        dockerCommand += ' ' + dockerConfig[modelID]['image']
+        dockerCommand += ' ' + dockerConfig[modelID]['command']
+
+        print('dockerCommand:\n', dockerCommand)
+        print('Length of docker command: ' + str(len(dockerCommand)))
+        os.system(dockerCommand)
+    
+    # listOfFilePathsOrFolder is a list of files
+    else:
+        nBatches = (len(listOfFilePathsOrFolder) + nFilesPerBatch - 1) // nFilesPerBatch
+        print('Number of batches:', nBatches)
+
+        # Loop over batches
+        for batchIx in range(nBatches):
+            print('Batch:', batchIx)
+            batchStartIx = batchIx * nFilesPerBatch
+            batchEndIx = min((batchIx + 1) * nFilesPerBatch, len(listOfFilePathsOrFolder))
+            batchFilePaths = listOfFilePathsOrFolder[batchStartIx:batchEndIx]
+
+            # Construct the Docker run command with bind mounts for each file
+            inputMounts = ''
+            for filePath in batchFilePaths:
+                fileName = os.path.basename(filePath)
+                inputMounts += ' -v ' + filePath + ':' + dockerConfig[modelID]['inputDir'] + '/' + fileName # + '/' is important here
+
+            dockerCommand += inputMounts
+            dockerCommand += ' -v ' + outputDir + ':' + dockerConfig[modelID]['outputDir']
+            dockerCommand += ' ' + dockerConfig[modelID]['options']
+            dockerCommand += ' ' + dockerConfig[modelID]['image']
+            dockerCommand += ' ' + dockerConfig[modelID]['command']
+
+            print('dockerCommand:\n', dockerCommand)
+            print('Length of docker command: ' + str(len(dockerCommand)))
+            os.system(dockerCommand)
+
+        
+
+
+    
 
 
 
@@ -294,15 +328,58 @@ def postProcessResults(modelID, outputRootDir, removeTemporaryResultFiles=False)
 ################################################################################################################
 ################################################################################################################
 
-# Create temp outputRootDir
-os.makedirs(outputRootDir, exist_ok=True)
+if __name__ == "__main__":
 
-# Run model
-#getModelResults(modelID, outputRootDir, inputDir) # Pass folder
-getModelResults(modelID, outputRootDir, filePaths) # Pass list of files
 
-# Post process results
-postProcessResults(modelID, outputRootDir)
+    # On Windows calling this function is necessary.
+    # On Linux/OSX it does nothing.
+    freeze_support()
+
+    ## Parse arguments
+    parser = argparse.ArgumentParser(description='Identify birds in audio files with various models.')
+
+    parser.add_argument('--modelID', type=str, metavar='', default=modelID, help='Model ID. Defaults to ' + modelID)
+    parser.add_argument('--outputRootDir', type=str, metavar='', default=outputRootDir, help='Output root directory. Defaults to ' + outputRootDir)
+
+    parser.add_argument('--inputDirOrTextFilePath', type=str, metavar='', default=inputTextFilePath, help='Input directory or path of text file with list of file paths. Defaults to ' + inputTextFilePath)
+    #parser.add_argument('--filePaths', nargs='+', help='List of file paths', required=False)
+
+    args = parser.parse_args()
+
+    ## Assign arguments to variables
+    modelID = args.modelID
+    outputRootDir = args.outputRootDir
+
+    inputDirOrTextFilePath = args.inputDirOrTextFilePath
+    if os.path.isfile(inputDirOrTextFilePath):
+        with open(inputDirOrTextFilePath, 'r') as file:
+            filePaths = file.readlines()
+            listOfFilePathsOrFolder = [x.strip() for x in filePaths]
+            nFilesToProcess = len(listOfFilePathsOrFolder)
+            print('Number of files to process:', nFilesToProcess)
+    else:
+        listOfFilePathsOrFolder = inputDirOrTextFilePath
+        # Todo: check if inputDirOrTextFilePath is a folder
+
+
+    #print('listOfFilePathsOrFolder', listOfFilePathsOrFolder)
+
+    # inputDir = args.inputDir
+    # filePaths = args.filePaths
+
+
+
+    ## Create temp outputRootDir
+    # Append '/' to outputRootDir if not already there
+    if outputRootDir[-1] != '/':
+        outputRootDir += '/'
+    os.makedirs(outputRootDir, exist_ok=True)
+
+    # Run model
+    getModelResults(modelID, outputRootDir, listOfFilePathsOrFolder)
+
+    # Post process results
+    postProcessResults(modelID, outputRootDir)
     
     
 
